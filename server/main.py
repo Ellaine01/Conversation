e53904fcce1956a51464a8caa6e57b75323ec227
nao28 <naomi.ezaki28@gmail.com>
2018-09-24 14:58:26 +0800

    added files

---
+++ server/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+template_dir = '../client/templates'
+app = Flask(__name__, template_folder=template_dir)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+# Routes--------------------------------------
+@app.route('/')
+def home():
+    # return render_template('login.html')
+    return "Hello"
+
+@app.route('/session')
+def sessions():
+    return render_template('session.html')
+
+#---------------------------------------------
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET','POST']):
+    print('received my event: '+str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', debug=True)
